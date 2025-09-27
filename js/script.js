477475fd2bbc5f2be31dea96766521430e23287b
Adib-P <madib0370@gmail.com>
2025-09-27 15:03:39 +0700

    Finally Done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 welcomMessage()
 
 function welcomMessage() {
-    let username = prompt("Enter Your Name:")
+    let username = prompt("Enter Your Name:");
     if (username) {
-        alert(`Welcome to my Portofolio, ${username}!` );
+        document.getElementById("username").innerText = username;
     } else {
-        alert(`Welcome to my Portofolio`)
+        alert("Welcome to my Portofolio");
     }
+};
+
+function validateForm() {
+    let firstname = document.getElementById("first-name").value;
+    let lastname = document.getElementById("last-name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    console.log(firstname, lastname, email, subject, message )
+    if (firstname === "" || lastname === "" || email === "" || subject === "" || message === "") {
+        alert("Please fill in all required fields.");
+        } else {
+            alert("Thank you for your message, " + firstname + lastname + "! We will get back to you shortly.");
+        }
 };
